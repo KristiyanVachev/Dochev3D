0ea83352b1f186f0d2b6abf41251503e86140781
Kristiyan Vachev <typhon04@gmail.com>
2015-04-21 18:15:08 +0300

    First change

---
+++ js/GetPixels.js
@@ -0,0 +1,25 @@
+﻿function SetHeight() {
+    var $height = $(window).height() - 60; //getting the screen height, without the 60px reserverd for the navigation
+
+    $(".slide").css("height", $height); //setting the height of the slide
+
+    for (var i = 1; i <= 4; i++) {
+
+        var $ctnHeight = $("#content" + i).height(); //getting the height of the content
+
+        if ($ctnHeight < $height) {
+            var $paddng = ($height - $ctnHeight) / 2; //calculating the margin needed
+            $("#content" + i).css("margin-top", $paddng);
+        }
+        else {
+            $(".slide").css("height", $ctnHeight + 60);
+        }
+    }
+
+}
+
+//$(window).on('resize', function (e) {
+//    SetHeight();
+//});
+
+SetHeight();
